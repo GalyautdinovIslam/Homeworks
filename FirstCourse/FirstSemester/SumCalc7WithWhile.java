4f743e18a72ff5819545c90a1e3f454364d4a6a5
GalyautdinovIslam <galyautdinovislam@gmail.com>
2021-06-06 23:22:52 +0300

    SumCalc7WithWhile was added.

--- FirstCourse/FirstSemester/SumCalc7WithWhile.java
+++ FirstCourse/FirstSemester/SumCalc7WithWhile.java
@@ -1,15 +1,17 @@
 package FirstSemester;
 
-public class SumCalc7 {
+public class SumCalc7WithWhile {
     public static void main(String[] args) {
+
         if (args.length == 1) {
             int n = Integer.parseInt(args[0]);
 
             if (n > 0) {
                 double s = 0;
 
-                for (int i = 1; i < n + 1; i++) {
-                    s -= (Math.pow(-1, i)) / (Math.pow(2 * i - 1, 2));
+                while (n > 0) {
+                    s = s - (Math.pow(-1, n)) / (Math.pow(2 * n - 1, 2));
+                    n = n - 1;
                 }
 
                 System.out.println(s);
